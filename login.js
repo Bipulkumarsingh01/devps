4b26985467c6061f1b5807786aae2eeaee92ad0c
Bipulkumarsingh01 <Bipulkumar.ec19@bmsce.ac.in>
2022-12-08 22:57:40 +0530

    first commit

--- login.js
+++ login.js
@@ -3,12 +3,16 @@ const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
 signUpButton.addEventListener('click', () => {
-    console.log('firing');
-
 	container.classList.add("right-panel-active");
 });
 
 signInButton.addEventListener('click', () => {
-    console.log('firing');
 	container.classList.remove("right-panel-active");
 });
+
+
+
+
+
+
+
